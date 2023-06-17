e949ace46b1e4760eb7a1a24b2bab613467b4fdf
Diego Figueroa <diegofigueroa79@gmail.com>
2023-06-17 13:02:09 -0700

    fix resizing issues

--- src/components/home/header.jsx
+++ src/components/home/header.jsx
@@ -4,12 +4,13 @@ export default function Header() {
     return (
       <header>
         <nav>
-          <span></span>
+          <span className={styles.spacer}></span>
           <span className={styles.home}><a href='/portfolio-frontend/' className={styles.nostyle}>Home</a></span>
           <span><a href='#about' className={styles.nostyle}>About</a></span>
-          <span><a href='#technologies' className={styles.nostyle}>Technologies</a></span>
+          <span className={styles.technology}><a href='#technologies' className={styles.nostyle}>Technologies</a></span>
+          <span className={styles.tech}><a href='#technologies' className={styles.nostyle}>Tech</a></span>
           <span><a href='#projects' className={styles.nostyle}>Projects</a></span>
-          <span></span>
+          <span className={styles.spacer}></span>
         </nav>
       </header>
     )
