7dd94b38f648cdbdb21fd0cdb02fe7239a904f1f
Diego Figueroa <diegofigueroa79@gmail.com>
2023-06-18 23:05:10 -0700

    add card templates for projects

--- src/components/home/projects.jsx
+++ src/components/home/projects.jsx
@@ -1,4 +1,5 @@
 import styles from '../../pages/home/home.module.scss'
+import goKartPhoto from '../../resources/go-kart-crazy.png'
 
 
 export default function Projects() {
@@ -10,17 +11,50 @@ export default function Projects() {
             </div>
             <div className={styles.project}>
                 <div className={styles.module}></div>
-                <div className={styles.mainModule}>Project</div>
+                <div className={styles.mainModule}>
+                    <div className={styles.projectPhoto}>
+                        <img src={goKartPhoto} alt="gokart" />
+                    </div>
+                    <div className={styles.projectDescription}>
+                        <span>Fun simple go kart game! Built with Unity game engine.</span>
+                        <span>Integrated with React via the react-unity-webgl library!</span>
+                        <span></span>
+                        <span>Frontend: React, SCSS, react-unity-webgl library</span>
+                        <span>Game Engine: Unity 3D</span>
+                    </div>
+                </div>
                 <div className={styles.module}></div>
             </div>
             <div className={styles.project}>
                 <div className={styles.module}></div>
-                <div className={styles.mainModule}>Project</div>
+                <div className={styles.mainModule}>
+                    <div className={styles.projectPhoto}>
+                        <img src={goKartPhoto} alt="gokart" />
+                    </div>
+                    <div className={styles.projectDescription}>
+                        <span>Fun simple go kart game! Built with Unity game engine.</span>
+                        <span>Integrated with React via the react-unity-webgl library!</span>
+                        <span></span>
+                        <span>Frontend: React, SCSS, react-unity-webgl library</span>
+                        <span>Game Engine: Unity 3D</span>
+                    </div>
+                </div>
                 <div className={styles.module}></div>
             </div>
             <div className={styles.project}>
                 <div className={styles.module}></div>
-                <div className={styles.mainModule}>Project</div>
+                <div className={styles.mainModule}>
+                    <div className={styles.projectPhoto}>
+                        <img src={goKartPhoto} alt="gokart" />
+                    </div>
+                    <div className={styles.projectDescription}>
+                        <span>Fun simple go kart game! Built with Unity game engine.</span>
+                        <span>Integrated with React via the react-unity-webgl library!</span>
+                        <span></span>
+                        <span>Frontend: React, SCSS, react-unity-webgl library</span>
+                        <span>Game Engine: Unity 3D</span>
+                    </div>
+                </div>
                 <div className={styles.module}></div>
             </div>
         </div>
